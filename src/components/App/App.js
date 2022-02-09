1eab5beaef6cb91f73f18e4f2eb3ceb447bb4b42
Oleg <olego250@gmail.com>
2022-02-09 13:48:28 +0200

    Starter App

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <div className="container">
+                <h1>Todos async app</h1>
+            </div>
+
+        </div>
+    );
+}
+
+export default App;
